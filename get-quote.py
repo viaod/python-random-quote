83ce7b85cb39fb22c3494d091508e2b5d1105605
ctrl-vod <78152908+ctrl-vod@users.noreply.github.com>
2022-02-13 16:27:04 +0000

    added some features to get-quote.py and quotes to quote.txt

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,55 @@
+from code import interact
 import random
 
+# ask user if they want to insert quotes or see quotes 
+def start():
+  answer = input("Would you like to add a quote? y/n \t").lower().strip()
+
+  print()
+
+  if answer == "y":
+    insert()
+  elif answer == "n":
+    print("Now printing 3 random quotes...")
+    primary()
+  else:
+    print("Not really sure what that means... So, I'll print some quotes :)")
+    primary()
+
+
+# get quotes and print them 
 def primary():
-  print("Keep it logically awesome.")
+  #print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  last =  13 # or len(quotes) - 1
-  rnd = random.randint(0,last)
+  last = len(quotes) - 1
 
-  print(quotes[rnd])
+  # print 3 different random numbers 
+  randoms = random.sample(range(0,last), 3)
+  
+  print()
+
+  for num in randoms:
+    print(quotes[num], end="")
+
+  #rnd = random.randint(0,last)
+  #print(quotes[rnd])
+
+
+# add quote to file 
+def insert():
+  answer = input("What words of wisdom do you have?  ")
+
+  f = open("quotes.txt", 'a')
+  f.write(answer)
+  f.close()
+
+  print("All done!\n")
+  start()
+
 
 if __name__== "__main__":
-  primary()
+  start()
